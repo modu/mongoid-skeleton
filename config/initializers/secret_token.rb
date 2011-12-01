b9717e07993ed9296de25afa3965b0d89a3254bf
Tyler Brock <tyler.brock@gmail.com>
2011-12-01 14:17:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidSkeleton::Application.config.secret_token = '806f0c9c9cd597db2e7c9f3be6ec4a1542dc4ffec0dfa31d527efcfc43ee272844b802b8545cc77f93cc76d53787ad58e134754241423cba2f3c9f47d95ee219'
